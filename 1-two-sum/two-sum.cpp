ac3f5cf542e3d238f777878383098d3a50c258f4
Jay Mandaliya <108296276+clumsydemon@users.noreply.github.com>
2023-07-22 20:15:03 +0530

    Time: 15 ms (65.45%) | Memory: 11.1 MB (15.24%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // map<int,int> mpp;
-        // for(int i = 0;i<nums.size();i++){
-        //     int num = nums[i];
-        //     int needed = target - num;
-        //     if(mpp.find(needed) != mpp.end()){
-        //         return {mpp[needed],i};
-        //     }
-        //     mpp[num] = i;
-        // }
-        // return {-1,-1};       t.c = o(nlogn)
-
+        map<int,int> mpp;
         for(int i = 0;i<nums.size();i++){
-            for(int j = i+1;j<nums.size();j++){
-                if(nums[i] + nums[j] == target){
-                    return {i,j};
-                }
+            int num = nums[i];
+            int needed = target - num;
+            if(mpp.find(needed) != mpp.end()){
+                return {mpp[needed],i};
             }
+            mpp[num] = i;
         }
-        return {-1,-1};
+        return {-1,-1};     //  t.c = o(nlogn)
+
+        // for(int i = 0;i<nums.size();i++){
+        //     for(int j = i+1;j<nums.size();j++){
+        //         if(nums[i] + nums[j] == target){
+        //             return {i,j};
+        //         }
+        //     }                         
+        // }
+        // return {-1,-1};      t.c = o(n^2) approx
     } 
 };
