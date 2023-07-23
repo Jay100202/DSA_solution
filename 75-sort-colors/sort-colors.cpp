f206a63f2c9d4434400997de9acd9ef50fb7f053
Jay Mandaliya <108296276+clumsydemon@users.noreply.github.com>
2023-07-23 14:36:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (7.81%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int cnt_0 = 0;
-        int cnt_1 = 0;
-        int cnt_2 = 0;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] == 0){
-                cnt_0++;
+        // By Dutch National flag Algorithm
+
+        int low = 0, mid = 0,high = nums.size() - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
             }
-            else if(nums[i] == 1){
-                cnt_1++;
+            else if(nums[mid] == 1){
+                mid++;
             }
             else{
-                cnt_2++;
+                swap(nums[high],nums[mid]);
+                high--;
             }
         }
+    }                    // t.c = o(n)  s.c = o(1)
 
-        for(int i = 0;i<cnt_0;i++){
-            nums[i] = 0;
-        }
+    // int cnt_0 = 0;
+        // int cnt_1 = 0;
+        // int cnt_2 = 0;
+        // for(int i = 0;i<nums.size();i++){
+        //     if(nums[i] == 0){
+        //         cnt_0++;
+        //     }
+        //     else if(nums[i] == 1){
+        //         cnt_1++;
+        //     }
+        //     else{
+        //         cnt_2++;
+        //     }
+        // }
 
-        for(int i = cnt_0;i<cnt_0+cnt_1;i++){
-            nums[i] = 1;
-        }
+        // for(int i = 0;i<cnt_0;i++){
+        //     nums[i] = 0;
+        // }
 
-        for(int i = cnt_0+cnt_1;i<nums.size();i++){
-            nums[i] = 2;
-        }
-    }
+        // for(int i = cnt_0;i<cnt_0+cnt_1;i++){
+        //     nums[i] = 1;
+        // }
+
+        // for(int i = cnt_0+cnt_1;i<nums.size();i++){
+        //     nums[i] = 2;
+        // }           t.c = o(2n)  s.c = 0(1)           
 };
