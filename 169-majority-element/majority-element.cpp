a70c0b82cb3266783db1a6f25fd1e351495a55b8
Jay Mandaliya <108296276+clumsydemon@users.noreply.github.com>
2023-07-23 17:54:15 +0530

    Time: 21 ms (66.04%) | Memory: 19.6 MB (12.16%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+
+        // more voting algo 
+        int cnt = 0;
+        int el;
+        for(int i = 0;i<nums.size();i++){
+            if(cnt == 0){
+                cnt++;
+                el = nums[i];
+            }
+            else if(nums[i] == el){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+       
+        int cnt1 = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == el){
+                cnt1++;
+            }
+        }
+
+        if(cnt1 > nums.size()/2){
+            return el;
+        }
+
+        return -1;            // t.c = o(n) + o(n) the second o(n) is optional
+
         // brute force
 
         // for(int i = 0;i<nums.size();i++){
@@ -17,17 +47,18 @@ public:
         // }
         // return -1;  t.c = o(n^2) 
 
-        unordered_map<int,int> mpp;
-        for(int i = 0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
+        // unordered_map<int,int> mpp;
+        // for(int i = 0;i<nums.size();i++){
+        //     mpp[nums[i]]++;
+        // }
 
-        for(auto it:mpp){
-            if(it.second > nums.size()/2){
-                return it.first;
-            }
-        }
+        // for(auto it:mpp){
+        //     if(it.second > nums.size()/2){
+        //         return it.first;
+        //     }
+        // }
 
-        return -1;
+        // return -1;           // t.c = o(nlogn) + o(n) nlogn depends upon which map u r using
+        //                      // s.c = o(n)
     }
 };
