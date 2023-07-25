2d690861a4d5dcc21a9313b38f2c66f6aa52d103
Jay Mandaliya <108296276+Jay100202@users.noreply.github.com>
2023-07-25 14:59:46 +0530

    Time: 203 ms (59.36%) | Memory: 67 MB (39.21%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int cnt = 1;
-        int last_smaller = INT_MIN;
-        int longest = 0;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i]-1 == last_smaller){
-                cnt++;
-                last_smaller = nums[i];
-            }
-            else if(nums[i] != last_smaller){
-                cnt = 1;
-                last_smaller = nums[i];
-            }
-            longest = max(longest,cnt);
+    int longestConsecutive(vector<int>& arr) {
+
+        unordered_set<int> s;
+        int x,cnt=1,maxlen=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            s.insert(arr[i]);
         }
-       return longest;
+        for(auto it:s)
+        {
+            if(s.find(it-1)==s.end())
+            {
+                cnt=1;
+                x=it+1;
+                while(s.find(x)!=s.end())
+                {
+                    cnt++;
+                    x=x+1;
+                }
+                maxlen=max(maxlen,cnt);
+            }
+        }
+        return maxlen;
+    //     sort(nums.begin(),nums.end());
+    //     int cnt = 1;
+    //     int last_smaller = INT_MIN;
+    //     int longest = 0;
+    //     for(int i = 0;i<nums.size();i++){
+    //         if(nums[i]-1 == last_smaller){
+    //             cnt++;
+    //             last_smaller = nums[i];
+    //         }
+    //         else if(nums[i] != last_smaller){
+    //             cnt = 1;
+    //             last_smaller = nums[i];
+    //         }
+    //         longest = max(longest,cnt);
+    //     }
+    //    return longest;
     }
 };
