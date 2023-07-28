5192584c664f2f46c4874782c2a0ba27bead0d98
Jay Mandaliya <108296276+Jay100202@users.noreply.github.com>
2023-07-28 12:00:05 +0530

    Time: 12 ms (91.97%) | Memory: 13.3 MB (60.31%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,25 +1,74 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size(),n=matrix[0].size();
-        vector<int> col(n,0);
-        vector<int> row(m,0);
+    void setZeroes(vector<vector<int>>& arr) {
 
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(matrix[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
+        int m = arr.size();
+        int n = arr[0].size();
+        int col = 1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i][j]==0)
+                {
+                    arr[i][0] = 0;
+                    if(j!=0)
+                    {
+                        arr[0][j] = 0;
+                    }
+                    else
+                    {
+                        col = 0;
+                    }
                 }
             }
         }
-
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(row[i] || col[j]){
-                    matrix[i][j] = 0;
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(arr[i][j]!=0)
+                {
+                    if(arr[i][0]==0 || arr[0][j]==0)
+                    {
+                        arr[i][j] = 0;
+                    }
                 }
             }
         }
-    }
+        if(arr[0][0]==0)
+        {
+            for(int j=0;j<n;j++)
+            {
+                arr[0][j] = 0;
+            }
+        }
+        if(col==0)
+        {
+            for(int i=0;i<m;i++)
+            {
+                arr[i][0]= 0;
+            }
+        }
+        // int m=matrix.size(),n=matrix[0].size();
+        // vector<int> col(n,0);
+        // vector<int> row(m,0);
+
+        // for(int i = 0;i<m;i++){
+        //     for(int j = 0;j<n;j++){
+        //         if(matrix[i][j] == 0){
+        //             row[i] = 1;
+        //             col[j] = 1;
+        //         }
+        //     }
+        // }
+
+        // for(int i = 0;i<m;i++){
+        //     for(int j = 0;j<n;j++){
+        //         if(row[i] || col[j]){
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+    }    // t.c = o(n^2) // s.c = o(n+m)  
 };
