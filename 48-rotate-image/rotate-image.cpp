c467ca007eb4bf897ca5cfe3bf4d6e13c7fdb472
Jay Mandaliya <108296276+Jay100202@users.noreply.github.com>
2023-07-29 11:05:23 +0530

    Time: 4 ms (33.19%) | Memory: 7 MB (46.22%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        vector < vector < int >> ans(n, vector < int > (n, 0));
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                ans[j][n-i-1] = matrix[i][j];
+        // brute force 
+        // int n=matrix.size();
+        // vector < vector < int >> ans(n, vector < int > (n, 0));
+        // for(int i = 0;i<n;i++){
+        //     for(int j = 0;j<n;j++){
+        //         ans[j][n-i-1] = matrix[i][j];
+        //     }
+        // }
+        // matrix = ans;   T.C = O(n^2) S.C = O(n^2)
+
+        int n = matrix.size();
+        // step 1 transpose
+        for(int i = 0;i<n-1;i++){
+            for(int j = i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        matrix = ans;
+        
+        // step 2 reverse every row
+        for(int i = 0;i<n;i++){
+            // row is matrix[i]
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };
