09617f2a1d27cb1a9c978ae47f49a2b1ade0f8aa
Jay Mandaliya <108296276+Jay100202@users.noreply.github.com>
2023-07-31 15:03:30 +0530

    Time: 16 ms (53.75%) | Memory: 15.9 MB (63.47%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -25,18 +25,35 @@ public:
         // return ans;   t.c = o(n^2) s.c = o(1)
 
         // better solution 1
+        // unordered_map<int,int> mpp;
+        // vector<int> ans;
+        // int n = nums.size(); 
+        // for(int i = 0;i<n;i++){
+        //    mpp[nums[i]]++;
+        // }
+
+        // for(auto it:mpp){
+        //     if(it.second > n/3){
+        //         ans.push_back(it.first);
+        //     }
+        // }
+        // return ans;   t.c = o(2n)  s.c = o(n)  
+
+        // better solution 2
+        int n = nums.size();
         unordered_map<int,int> mpp;
         vector<int> ans;
-        int n = nums.size(); 
+        int mini = (int) (n/3) + 1;
         for(int i = 0;i<n;i++){
-           mpp[nums[i]]++;
-        }
-
-        for(auto it:mpp){
-            if(it.second > n/3){
-                ans.push_back(it.first);
+            mpp[nums[i]]++;
+            if(mpp[nums[i]] == mini){
+                ans.push_back(nums[i]);
             }
-        }\
+
+            if(ans.size() == 2){
+                break;
+            }
+        }
         return ans;
     }
 };
