de96dc959ea273312d2ddc7ffb47f1af16b110e8
Jay Mandaliya <108296276+Jay100202@users.noreply.github.com>
2023-08-05 14:17:28 +0530

    Time: 7 ms (80.36%) | Memory: 14 MB (5.03%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,15 +1,59 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int first = -1,last = -1;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] == target){
-                if(first == -1){
-                    first = i;
-                }
-                last = i;
+
+    int lowerBound(vector<int> arr, int n, int x) {
+        int low = 0,high = n-1;
+        int ans = n;
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(arr[mid] >= x){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
             }
         }
-        return {first,last};
+        return ans;
+    }
+
+    int upperBound(vector<int> &arr, int n, int x){
+        int low = 0,high = n-1;
+        int ans = n;
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(arr[mid] > x){
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        int lb = lowerBound(nums,n,target);
+        if(lb == n || nums[lb] != target){
+            return {-1,-1};
+        }
+        else{
+            return {lb, upperBound(nums,n,target) - 1};
+        }
+
+        // linear search
+        // int first = -1,last = -1;
+        // for(int i = 0;i<nums.size();i++){
+        //     if(nums[i] == target){
+        //         if(first == -1){
+        //             first = i;
+        //         }
+        //         last = i;
+        //     }
+        // }
+        // return {first,last};
     }
 };
