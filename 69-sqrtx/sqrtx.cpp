0ff63d5c4c2f7055b853106a510e021a7e1e8810
Jay Mandaliya <108296276+Jay100202@users.noreply.github.com>
2023-08-09 19:53:03 +0530

    Time: 5 ms (33.55%) | Memory: 5.9 MB (87.77%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,7 +1,42 @@
 class Solution {
 public:
     int mySqrt(int x) {
-       int n =  sqrt(x);
-       return n;
+    //    int n =  sqrt(x);
+    //    return n;
+
+    // int ans = 1;
+    // if(x == 0){
+    //     return 0;
+    // }
+    // for(int i = 0;i<x;i++){
+    //     if(i*i<=x){
+    //         ans = i;
+    //         continue;
+    //     }
+    //     else{
+    //         break;
+    //     }
+    // }
+    // return ans;
+    // }
+
+    long low = 1,high = x;
+    long ans = 1;
+    if (x == 0 || x == 1)
+         return x;
+    while(low <= high){
+        long mid = (low + high)/2;
+
+        if(mid * mid <= x){
+            ans = mid;
+            low = mid + 1;
+
+        }
+        else{
+            high = mid - 1;
+        }
+
+    }
+    return ans;
     }
 };
